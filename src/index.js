c25c2d5e7b1315066551b24e899a043a3a3ec823
dutchredhead <120199325+dutchredhead@users.noreply.github.com>
2023-02-03 13:56:19 +0100

    added current time with API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day}, ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#change-date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
